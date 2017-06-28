0b8bf21ec5bb418279ecb11a7545b982950eaa0a
Nicholas Nieman <njn17845@gmail.com>
2017-06-27 22:58:14 -0400

    add development tools

--- logic.js
+++ logic.js
@@ -1,14 +1,39 @@
-var layer1 = document.getElementById("l1"),
-	layer2 = document.getElementById("l2"),
-	layer3 = document.getElementById("l3"),
-	layer4 = document.getElementById("l4");
-
-function initParallax() {
-    layer1.style.top = (window.pageYOffset / 1.5) + "px";
-    layer2.style.top = (window.pageYOffset / 1.5) + "px";
-    layer3.style.top = (window.pageYOffset / 2) + "px";
-    layer4.style.top = (window.pageYOffset / 1.2) + "px";
-    layer4.style.right = (window.pageYOffset) + "px";
+var layer1    = document.getElementById("l1"),
+	layer2    = document.getElementById("l2"),
+	layer3   		= document.getElementById("l3"),
+	layer4 	 	    = document.getElementById("l4"),
+	astronaut 		= document.getElementById("astronaut"),
+	bio 		    = document.getElementById("bio"),
+	devTools 		= document.getElementById("dev-tools");
+	
+	
+function initScrollEffects() {
+   	parallax(layer1, 1.5);
+   	parallax(layer2, 1.5);
+   	parallax(layer3, 2);
+    spaceFloat();
+    slideUp(bio);
+    // slideUp(devTools);
 }
 
-window.addEventListener("scroll", initParallax);
+function parallax(layer, speed) {
+	layer.style.top = (window.pageYOffset / speed) + "px";
+}
+
+function spaceFloat() {
+	parallax(layer4, 1.2);
+    layer4.style.right = (window.pageYOffset) + "px";
+    astronaut.style.width = (1800 - pageYOffset)/6 +"px";
+}
+
+function slideUp(element) {
+	var windowHeight = window.innerHeight;
+	var windowBottom = windowHeight + window.pageYOffset;
+	console.log(windowBottom);
+	if ((windowBottom >= element.offsetTop)&&(element.classList.contains("in-view") === false)){
+		console.log(element);
+		element.className += "in-view";
+	}
+}
+
+window.addEventListener("scroll", initScrollEffects);

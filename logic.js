b1ba83b1a505f8a4285e372ce0a6f38757c5dbbf
Nicholas Nieman <njn17845@gmail.com>
2017-06-28 18:38:01 -0400

    add animations

--- logic.js
+++ logic.js
@@ -1,19 +1,18 @@
 var layer1    = document.getElementById("l1"),
 	layer2    = document.getElementById("l2"),
-	layer3   		= document.getElementById("l3"),
-	layer4 	 	    = document.getElementById("l4"),
-	astronaut 		= document.getElementById("astronaut"),
-	bio 		    = document.getElementById("bio"),
-	devTools 		= document.getElementById("dev-tools");
-	
+	layer3    = document.getElementById("l3"),
+	layer4 	  = document.getElementById("l4"),
+	astronaut = document.getElementById("astronaut"),
+	bio 	  = document.getElementById("bio"),
+	devTools  = document.getElementById("dev-tools");
 	
 function initScrollEffects() {
    	parallax(layer1, 1.5);
    	parallax(layer2, 1.5);
    	parallax(layer3, 2);
     spaceFloat();
-    slideUp(bio);
-    // slideUp(devTools);
+    slideIn(bio);
+    slideIn(devTools);
 }
 
 function parallax(layer, speed) {
@@ -26,13 +25,11 @@ function spaceFloat() {
     astronaut.style.width = (1800 - pageYOffset)/6 +"px";
 }
 
-function slideUp(element) {
-	var windowHeight = window.innerHeight;
-	var windowBottom = windowHeight + window.pageYOffset;
-	console.log(windowBottom);
-	if ((windowBottom >= element.offsetTop)&&(element.classList.contains("in-view") === false)){
-		console.log(element);
-		element.className += "in-view";
+function slideIn(element) {
+	var windowBottom = window.innerHeight + window.pageYOffset;
+	var elementBottom = element.clientHeight + element.offsetTop;
+	if ((windowBottom >= elementBottom)&&(element.classList.contains("slide-left") === false)){
+		element.className += "slide-left";
 	}
 }
 
